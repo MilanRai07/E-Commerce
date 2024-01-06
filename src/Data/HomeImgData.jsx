970badd54b43a5e777175ce0a568c7af034fc428
milanrai <milanraiyt07@gmail.com>
2024-01-06 23:03:07 +0545

    HomePage Slider is created

--- src/Data/HomeImgData.jsx
+++ src/Data/HomeImgData.jsx
@@ -4,17 +4,17 @@ import shop3 from '../assets/shop3.jpg';
 export const Images = [
     {
         id: 0,
-        'img':'shop1.jpg',
+        'img':shop1,
         'title': 'Be an artist. Create your own art.'
     },
     {
         id: 1,
-        img: {shop2},
+        'img': shop2,
         'title': 'Get Enroll with us. Stay connected.'
     },
     {
         id: 2,
-        'img': {shop3},
+        'img': shop3,
         'title': 'Choose your instrument with us.'
     }
 ]
--- src/Home/HomeIndex.jsx
+++ src/Home/HomeIndex.jsx
@@ -1,40 +1,53 @@
 import HomeImage from "./HomeImage";
-import {Images} from '../Data/HomeImgData';
-import { createContext } from "react";
+import { Images } from '../Data/HomeImgData';
+import { useState } from "react";
 import '../scss/HomeImage.scss';
-import {ReactComponent as Left} from '../assets/left.svg';
-import {ReactComponent as Right} from '../assets/right.svg';
-const ParentData=createContext();
-const HomeIndex=()=>{
-    const contextValue={
-      
-    }
-  return(
-    <>
-    <div className="home-container">
-      <Left className="left-slide"/>
-    <ParentData.Provider value={contextValue}>
-      <div className="homeimg-container">
-      {Images.slice(0,1).map((ele,index)=>{
-        const { id, img, title } = ele;
-        return(
-          <div key={index}>
-            <HomeImage
-              Id={id}
-              Img={img}
-              Title={title}
-            />
-          </div>
-        )
-      })
+import { ReactComponent as Left } from '../assets/left.svg';
+import { ReactComponent as Right } from '../assets/right.svg';
 
-      }
+const HomeIndex = () => {
+  const [current, setCurrent] = useState(2);
+
+  const LeftSlide = () => {
+    if (current == 0) {
+      setCurrent(2);
+    } else {
+      setCurrent(current - 1);
+    }
+  }
+  const RightSlide = () => {
+    if (current == 2) {
+      setCurrent(0);
+    } else {
+      setCurrent(current + 1);
+    }
+  }
+
+  return (
+    <>
+      <div className="home-container">
+        <Left className="left-slide" onClick={LeftSlide} />
+          <div className="homeimg-container">
+            {Images.map((ele, index) => {
+              const { id, img, title } = ele;
+              return (
+                <div key={index} className={index === current ? 'active-img' : 'passive-img'}>
+                  {index === current && (
+                    <HomeImage
+                      Id={id}
+                      Img={img}
+                      Title={title}
+                    />
+                  )}
+                </div>
+              )
+            })
+
+            }
+          </div>
+        <Right className="right-slide" onClick={RightSlide} />
       </div>
-       </ParentData.Provider>
-       <Right className="right-slide"/>
-       </div>
     </>
   )
 }
 export default HomeIndex;
-export {ParentData};
