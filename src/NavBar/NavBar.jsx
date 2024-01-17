60cf578ad52fbd3dc443c3176043410d77edeecb
milanrai <milanraiyt07@gmail.com>
2024-01-17 09:01:05 +0545

    sticky navbar is created when scroll

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../scss/NavBar.scss';
 import { NavLink, Outlet } from "react-router-dom";
 import { ReactComponent as Logo } from "../assets/logo.svg";
 import { ReactComponent as HamBurger } from "../assets/Ham.svg";
 import { useState } from 'react';
 import HamBurgerMenu from './HamburgerMenu';
+import { ParentData } from './NavBarIndex';
 const NavBar = () => {
     const [showham, setShowHam] = useState(false);  //for showing and not showng hamburger nav menu
+    const context=useContext(ParentData); //context api consuming
+    const {isNavFixed}=context; //destructuring context
+
+
     const ToggleHamburger = () => {  //toggle hamburger menu nav
         setShowHam(!showham);
     }
     return (
         <>
-            <div className="navbar-container">
+            <div className={`navbar-container ${isNavFixed? 'fixed':''}`}>
                 <div className="navbar-logo">
                     <Logo className='logo' />
                 </div>
@@ -53,11 +58,13 @@ const NavBar = () => {
 
                     </ul>
                 </div>
-            </div>
-            {showham ?        //toggle hammenu by hamburger icon
-                <HamBurgerMenu /> :
+                {showham ?        //toggle hammenu by hamburger icon
+                <HamBurgerMenu /> 
+                :
                 ""
             }
+            </div>
+            
             <Outlet />
         </>
     )
--- src/NavBar/NavBarIndex.jsx
+++ src/NavBar/NavBarIndex.jsx
@@ -1,11 +1,29 @@
 import NavBar from "./NavBar";
-import React from 'react';
-
+import React, { createContext, useEffect, useState } from 'react';
+const ParentData=createContext();
 const NavBarIndex = () => {
+  const [isNavFixed,setIsNavFixed]=useState(false);
+  const handleScroll=()=>{
+    const scrollPosition=window.scrollY;
+    const triggerPosition=450;
+    setIsNavFixed(scrollPosition>triggerPosition);
+  }
+  useEffect(()=>{
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+  },[])
+  const value={
+    isNavFixed:isNavFixed,
+  }
   return (
     <>
+    <ParentData.Provider value={value}>
       <NavBar />
+    </ParentData.Provider>
     </>
   )
 }
 export default NavBarIndex;
+export {ParentData};
