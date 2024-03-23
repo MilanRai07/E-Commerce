4cf78d7046476fdf8f48ef84ac13e18745fa7b3e
milanrai <milanraiyt07@gmail.com>
2024-03-23 19:47:35 +0545

    error in input fields fixed

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import '../../scss/ErrorPage.scss';
 const ErrorPage = () => {
   return (
     <>
-      <div className='error'>
+      <div className='error-page'>
         <div>
           <h1>404</h1>
           <p>Page unavailable</p>
